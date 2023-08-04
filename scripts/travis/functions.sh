676767a046dd1f8b4542a854f4c2df4736f6569b
batistajon <batista.jonathas@gmail.com>
2023-08-04 08:45:49 -0300

    BIT-4919: Test comment adjust

--- scripts/travis/functions.sh
+++ scripts/travis/functions.sh
@@ -2,14 +2,13 @@
 
 info() {
   printf " [info] %s\n\n" "$*" >&2
-  return
+  return 0
 }
 
 is_draft() {
     local GITHUB_TOKEN=$1
     local TRAVIS_PULL_REQUEST=$2
-
-    IS_DRAFT=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/batistajon/drupal-book/pulls/${TRAVIS_PULL_REQUEST}" | jq '.draft')
+    local IS_DRAFT=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/batistajon/drupal-book/pulls/${TRAVIS_PULL_REQUEST}" | jq '.draft')
     # IS_DRAFT=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/colibrigroup/CMS-Drupal-ECOM/pulls/${TRAVIS_PULL_REQUEST}" | jq '.draft')
 
     if $IS_DRAFT
